f83112cbd4a1e006fd932706171c317420076896
omar <chahbar.omar@hotmail.com>
2019-08-30 22:03:21 -0400

    reverting test changes

--- src/landingPage/LandingPage.jsx
+++ src/landingPage/LandingPage.jsx
@@ -9,14 +9,14 @@ const LandingPage = () => (
         <div className="container">
             <center>
                 <Fade left>
-                    <h1 className="bigText">Hii, Esraa</h1>
+                    <h1 className="bigText">Hi, my name is <span className="emphasis">OMAR CHAHBAR</span></h1>
                 </Fade>
                 <Fade left delay={1000}>
-                    <h2 className="subTitle">{"You're the best <3"}</h2>
+                    <h2 className="subTitle">I'm a Full Stack Software Engineer.</h2>
                 </Fade>
             </center>
         </div>
-        {/*<Arrows/>*/}
+        <Arrows/>
     </div>
 );
 
