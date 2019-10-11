4e61da90ef13009d6bcb358c5844e6a4fb0ef4ea
omar <chahbar.omar@hotmail.com>
2019-10-10 22:55:50 -0400

    renaming a project

--- src/projects/Project.jsx
+++ src/projects/Project.jsx
@@ -9,7 +9,7 @@ const Project = () => (
 const projects = [
     {
         label: "Featured Project",
-        title: "Marcotte Physiotherapy",
+        title: "Marcotte Physio",
         details: "This is an application I designed and built for a physiotherapy clinic during my third year at Western University. The website brings the benefits of going to a physiotherapist to the comfort of one's home by providing features like a login system, video conferencing, online tests that the patient can take, and an exercise player.",
         languages: ["MongoDB", "Node", "Express", "EmberJS"],
         className: "project-img marcotte",
